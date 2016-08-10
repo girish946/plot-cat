3d2ec640ff6510929abc1884a1e774af43280e18
girish joshi <girish946@gmail.com>
2016-08-10 18:17:51 +0530

    docstrings added

--- plotcat/plotcat.py
+++ plotcat/plotcat.py
@@ -8,8 +8,12 @@ import random
 
 class plotter:
 
+  """plotter initiates a matplotlib plot. This plot is used to plot the serial input."""
+
   def __init__(self, number_of_samples = 100, total_plots = 1,y_low_lim = 0, y_high_lim = 1024):
 
+    """initializes the figure with the specified number of subplots (arg: total_plots)
+    """
     self.fig = pylab.figure(1)
     self.currentAxis = pylab.arange(0, number_of_samples, 1)
 
@@ -25,16 +29,22 @@ class plotter:
     self.manager = pylab.get_current_fig_manager()
     self.timer = self.fig.canvas.new_timer(interval = 10)
 
-
-
   def set_call_back(self, func):
 
+    """sets callback function for updating the plot.
+    in the callback function implement the logic of reading of serial input
+    also the further processing of the signal if necessary has to be done in this
+    callbak function."""
     self.timer.add_callback(func)
     self.timer.start()
     pylab.show()
 
 
   def plot_self(self, func):
+
+    """define your callback function with the decorator @plotter.plot_self.
+    in the callback function set the data of lines 
+    in the plot using self.lines[i][j].set_data(your data)"""
     def func_wrapper():
       func(self)
       try:
