268aa1fe091446def42fe70d672526d7b13064d3
girish946 <girish946@gmail.com>
2016-10-25 19:24:21 +0530

    each plot can have different x,y limits

--- plotcat/plotcat.py
+++ plotcat/plotcat.py
@@ -10,23 +10,35 @@ class plotter:
     """plotter initiates a matplotlib plot. This plot is used to plot the
     serial input."""
 
-    def __init__(self, number_of_samples=100, total_plots=1, rows=1,
-                 cols=1, y_low_lim=0, y_high_lim=1024,
+    def __init__(self, number_of_samples=[100], total_plots=1, rows=1,
+                 cols=1, y_low_lim=[0], y_high_lim=[1024],
                  plot_lines=[1], names=['serial-graph'], time_interval=10):
 
         """initializes the figure with the specified number of subplots
            (arg: total_plots)
         """
         self.fig = pylab.figure(1)
-        self.currentAxis = pylab.arange(0, number_of_samples, 1)
-
+        self.currentAxis = []
+        
         self.plots = []
         self.lines = []
+        if not len(number_of_samples) == total_plots:
+            number_of_samples = [100 for i in range(total_plots)]
+
         if not len(names) == total_plots:
             names = ['serial-graph' for i in range(total_plots)]
 
+        if not len(y_low_lim) == total_plots:
+            y_low_lim = [0 for i in range(total_plots)]
+
+        if not len(y_high_lim) == total_plots:
+            y_high_lim = [1024 for i in range(total_plots)]
+
         if not len(plot_lines) == total_plots:
             plot_lines = [1 for i in range(total_plots)]
+
+        for i in range(total_plots):
+            self.currentAxis.append(pylab.arange(0, number_of_samples[i], 1))
 
         count = 1
         for i in range(rows):
@@ -35,11 +47,13 @@ class plotter:
                 new_plot = self.fig.add_subplot(((rows * 100) + (cols * 10)
                                                  + count))
                 for k in range(plot_lines[count-1]):
-                    new_line = new_plot.plot(self.currentAxis,
-                                         [random.randint(y_low_lim,
-                                                         y_high_lim)
+
+                    #print(number_of_samples, y_low_lim, y_high_lim)
+                    new_line = new_plot.plot(self.currentAxis[count-1],
+                                         [random.randint(y_low_lim[count-1],
+                                                         y_high_lim[count-1])
                                           for i in
-                                          range(0, number_of_samples)])
+                                          range(0, number_of_samples[count-1])])
                     self.lines.append(new_line)
 
                 pylab.title(names[count-1])
