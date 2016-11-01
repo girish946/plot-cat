91327851841f0d5a438e444a82e286e49fd76dc2
girish joshi <girish946@gmail.com>
2016-11-01 09:59:17 +0530

    minor changes

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ import os
 from setuptools import setup
 
 setup(
-    name = 'plotcat' ,
-    version = '1.0.1' ,
-    author = 'girish joshi' ,
-    author_email = 'girish946@gmail.com' ,
-    description = ("""tool to plot live serial input""") ,
+    name = 'plotcat',
+    version = '1.0.1',
+    author = 'girish joshi',
+    author_email = 'girish946@gmail.com',
+    description = ("""tool to plot live serial input"""),
     packages = ['plotcat'], 
     install_requires = ['matplotlib', 'pyserial'], 
-    keywords = 'serial input plotting realtime data matplotlib Raspberry_pi ' ,
+    keywords = 'serial input plotting realtime data matplotlib Raspberry_pi ',
     scripts = ['live_plot.py'], 
     long_description = """tool to plot live serial input""",
     
